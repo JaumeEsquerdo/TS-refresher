08666f6ed80732373c12ed4e0c24b05a20e0af49
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-10-28 23:16:07 +0100

    iniciar proyecto ts-refresher con primer archivo y tipos

---
+++ first-app.ts
@@ -0,0 +1,12 @@
+// let userName: string;
+// userName = "Julio";
+
+let userName = "Julio"; // inferido
+
+let userAge = 34;
+
+let isValid = true;
+
+//string, number, boolean
+
+// para compilar usar en la terminal $ npx tsc carpeta-que-quieras
