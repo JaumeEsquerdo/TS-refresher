b77e6455df3eda8a6c96076b79a8ed13341f6fd2
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-10-30 19:35:21 +0100

    añadir uso de useState con ts

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,15 +1,45 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import reactImage from '../public/vite.svg'
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string,
+  description: string,
+  id: number
+}
+
 const App = () => {
+  const [goals, setGoals] = useState<CourseGoal[]>([])
+
+  const handleAddGoal = () => {
+    setGoals((prev) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!'
+      }
+      return [...prev, newGoal]
+
+    })
+  }
   return (
     <main>
       <Header image={{ src: reactImage, alt: 'A image of react' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title='Learn React and TS'  >
-        <p>Learning with a course how to use TS in a React project</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}  >
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+
     </main>
   );
 }
