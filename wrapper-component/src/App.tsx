1605ec46c5885f181f2b90850eca691035d3d8ef
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-03 20:47:22 +0100

    añadir componente button con tipado de props seguna button o a

--- wrapper-component/src/App.tsx
+++ wrapper-component/src/App.tsx
@@ -1,4 +1,5 @@
 import Input from './components/Input'
+import Button from './components/Button'
 import './App.css'
 
 function App() {
@@ -9,6 +10,12 @@ function App() {
       <h1>Let's get started</h1>
       <Input id='name' label='Your name' type='text' />
       <Input id='age' label='Your age' type='number' />
+      <p>
+        <Button >A button</Button>
+      </p>
+      <p>
+        <Button href='https://google.com'>A link</Button >
+      </p >
     </>
   )
 }
