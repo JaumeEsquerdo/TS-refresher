fa9f68f07149fa0b3178bca761f249b774257148
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-03 11:14:34 +0100

    unir dos tipos en uno para hacer no necesario tener alguna key en un contexto

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from '../components/CourseGoal'
 import { type CourseGoal as CGoal } from '../App';
+import InfoBox from './InfoBox';
+import type { ReactNode } from 'react';
 
 // CGoal viene del type CourseGoal exportado desde App
 type CourseGoalListProps = {
@@ -10,16 +12,26 @@ type CourseGoalListProps = {
 /* goals: un array de CourseGoal[]
 onDeleteGoal: una función (id: number) => void */
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+    if (goals.length === 0) {
+        return <InfoBox mode='hint'>You have no course goals yet. Start adding some!</InfoBox>
+    }
+
+    let warningBox: ReactNode;
+    if (goals.length >= 4)
+        warningBox = <InfoBox severity='medium' mode='warning'>You're collecting a lot of goals. Don't put to much on your plate</InfoBox>
     return (
-        <ul>
-            {goals.map((goal) => (
-                <li key={goal.id}>
-                    <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal} >
-                        <p>{goal.description}</p>
-                    </CourseGoal>
-                </li>
-            ))}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map((goal) => (
+                    <li key={goal.id}>
+                        <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal} >
+                            <p>{goal.description}</p>
+                        </CourseGoal>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
 
