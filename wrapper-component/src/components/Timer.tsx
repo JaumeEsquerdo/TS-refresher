0407e7a6c2be87109940f67d4c3a84df60d93494
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-06 19:42:20 +0100

    añadir useEffect con un setInterval para reducir el tiempo del Timer

--- wrapper-component/src/components/Timer.tsx
+++ wrapper-component/src/components/Timer.tsx
@@ -1,12 +1,24 @@
 import Container from "./UI/Container";
 import { type Timer as TimerProps } from "../store/timers-context";
+import { useState, useEffect } from "react";
 
 
 const Timer = ({ name, duration }: TimerProps) => {
+    const [remainingTime, setRemainingTime] = useState(duration * 1000) //se convierte x 1000 en milisegundos
+
+    useEffect(() => {
+        setInterval(function () {
+            setRemainingTime(prev => prev - 50)
+        }, 50)
+    }, [])
+
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+
     return (
         <Container as='article'>
             <h2>{name}</h2>
-            <p>{duration}</p>
+            <p><progress max={duration * 1000} value={remainingTime} /></p>
+            <p>{formattedRemainingTime}</p>
         </Container>
     );
 }
