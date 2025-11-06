7cdd8e731b5513d427f78a09e34d127f9bfe4999
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-07 00:03:04 +0100

    añadir useEffect al timer con un interval para limpiarlo con useRef

--- wrapper-component/src/components/Timer.tsx
+++ wrapper-component/src/components/Timer.tsx
@@ -1,15 +1,24 @@
 import Container from "./UI/Container";
 import { type Timer as TimerProps } from "../store/timers-context";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 const Timer = ({ name, duration }: TimerProps) => {
     const [remainingTime, setRemainingTime] = useState(duration * 1000) //se convierte x 1000 en milisegundos
 
+    const interval = useRef<number | null>(null)
+
+    if (remainingTime <= 0 && interval.current) {
+        clearInterval(interval.current)
+    }
+
     useEffect(() => {
-        setInterval(function () {
+        const timer = setInterval(function () {
             setRemainingTime(prev => prev - 50)
         }, 50)
+        interval.current = timer; // se le pasa el timer utilizado en el useEffect al interval.current para que lo pueda parar en el condicional de arriba si es necesario
+
+        return () => clearInterval(timer)
     }, [])
 
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
