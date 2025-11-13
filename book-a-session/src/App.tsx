863a2fcf46700c837a3883f6574f741489edd91c
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-13 13:32:42 +0100

    estructurar App.tsx y Main.tsx para envolver con SessionProvideer y BrowserRouter

--- book-a-session/src/App.tsx
+++ book-a-session/src/App.tsx
@@ -1,11 +1,29 @@
+import Button from './components/Button'
+import BookSession from './components/BookSession'
+import UpcomingSessions from './components/UpcomingSessions'
+import { useState } from 'react'
 
-import './App.css'
+/**
+ * App principal:
+ * - Demuestra el uso de <Button> (como botón y como Link)
+ * - Muestra modales condicionales según el estado
+ */
 
 function App() {
+  const [book, setBook] = useState(false);
+  const [upcoming, setUpcoming] = useState(false);
 
   return (
     <>
+      {/* renderiza un button normal */}
+      <Button onClick={() => setBook(true)}>Book Session</Button>
 
+      {/* renderiza un Link de React Router */}
+      <Button to='/sessions'>Go to sessions page</Button>
+
+      {/* al montar estos componentes, los modales se abren automáticamente */}
+      {book && <BookSession onClose={() => setBook(false)} />}
+      {upcoming && <UpcomingSessions onClose={() => setUpcoming(false)} />}
     </>
   )
 }
--- book-a-session/src/main.tsx
+++ book-a-session/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import { SessionsProvider } from './context/SessionsContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SessionsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SessionsProvider>
   </StrictMode>,
 )
